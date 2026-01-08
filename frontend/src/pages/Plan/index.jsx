a364be7c08a513041e2393523a4c7683831586f5
min10728 <btm147@naver.com>
2026-01-08 15:58:28 +0900

    메뉴 수정

---
+++ frontend/src/pages/Plan/index.jsx
@@ -0,0 +1,5 @@
+const Plan = () => {
+  return <div className="plan container">plan</div>
+}
+
+export default Plan
--- frontend/src/pages/Theme/index.jsx
+++ frontend/src/pages/Theme/index.jsx
@@ -1,7 +1,5 @@
-
-
 const Theme = () => {
-  return <div className="album container">테마분석</div>;
-};
+  return <div className="Theme container">테마분석</div>
+}
 
-export default Theme;
+export default Theme
