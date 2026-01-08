dae64c0ca553b6d5915b074da7204cd5fe7f0706
yoo-yongbin <mry7735@gmail.com>
2026-01-08 18:10:33 +0900

    feat: 여행지 추천 페이지 UI 추가

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,8 +5,8 @@ const pool = mysql.createPool({
   host: "localhost",
   port: 3307,
   user: "root",
-  password: "123456", // 본인 MySQL 비밀번호로 변경
-  database: "trip-db",
+  password: "1234", // 본인 MySQL 비밀번호로 변경
+  database: "tripy",
   waitForConnections: true,
   connectionLimit: 10,
 })
