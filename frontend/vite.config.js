e2090979907539c9a5d1c319bde6837890dacdac
min10728 <btm147@naver.com>
2026-01-20 18:53:43 +0900

    setting upload file url 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -19,4 +19,4 @@ export default defineConfig({
       "/img": "http://localhost:5000",
     },
   },
-});
+})
